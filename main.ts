77bb2fe1ebbb9de773d89142f53119765eb35c9d
soft43 <95017406+soft43@users.noreply.github.com>
2021-11-27 13:23:15 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,24 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.startEffect(effects.confetti, randint(100, 600))
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(2)
+mySprite = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 d 4 c . . 
+    . . . . b 5 5 1 f f d d 4 4 4 b 
+    . . . . b 5 5 d f b 4 4 4 4 b . 
+    . . . b d 5 5 5 5 4 4 4 4 b . . 
+    . . b d d 5 5 5 5 5 5 5 5 b . . 
+    . b d d d d 5 5 5 5 5 5 5 5 b . 
+    b d d d b b b 5 5 5 5 5 5 5 b . 
+    c d d b 5 5 d c 5 5 5 5 5 5 b . 
+    c b b d 5 d c d 5 5 5 5 5 5 b . 
+    . b 5 5 b c d d 5 5 5 5 5 d b . 
+    b b c c c d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Player)
+game.splash("463 in your workspace!")
